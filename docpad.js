a3faf430a9ee944158ddf3a15a3a694765e9b8e8
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-08-19 12:04:33 +0200

    Update AlloyEditor to v.0.5.0

--- docpad.js
+++ docpad.js
@@ -27,7 +27,7 @@ module.exports = {
 
         alloyeditor: {
             // AlloyEditor version
-            version: '0.4.0'
+            version: '0.5.0-alpha'
         },
 
         /* -----------------------------
