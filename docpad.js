809cc573e7b71f525d464c3913e63db5123594ca
Iliyan Peychev <iliyan.peychev@gmail.com>
2016-06-11 09:20:21 +0200

    Add update to AlloyEditor 1.2.2 page

--- docpad.js
+++ docpad.js
@@ -15,7 +15,7 @@ module.exports = {
 
         alloyeditor: {
             // AlloyEditor version
-            version: '1.2.1'
+            version: '1.2.2'
         },
 
         /* -----------------------------
